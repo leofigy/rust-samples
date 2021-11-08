bf05a704cf91d9d4b2cc7a5fbc9f905f68a6fcb6
leofigy <angel.fig@yandex.com>
2021-11-08 12:54:38 -0600

    adding from examples

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,5 @@
+use crate::List::*;
+
 enum Action{
     Drive,
     Turn(Direction),
@@ -15,6 +17,40 @@ enum WebEvent {
     KeyPress(char),
     Paste(String),
     Click {x: i64, y: i64 },
+}
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil
+}
+
+impl List{
+    fn new() -> List {
+        Nil
+    }
+
+    fn add(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32{
+
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{} , {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
 }
 
 fn inspect(event: WebEvent){
@@ -63,4 +99,13 @@ fn main() {
     inspect(click);
     inspect(load);
     inspect(unload);
+
+    let mut list = List::new();
+    list = list.add(1);
+    list = list.add(2);
+    list = list.add(3);
+
+    println!("Linked list : {}", list.len());
+    println!("{}", list.stringify());
+
 }
